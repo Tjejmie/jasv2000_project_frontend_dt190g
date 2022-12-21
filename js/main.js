f1027ada7522e66136e7ce7193b11edbcde44eee
Tjejmie <jamie_svanberg@hotmail.com>
2022-12-21 16:00:53 +0100

    Teaminformation is showing when you click on the team

--- js/main.js
+++ js/main.js
@@ -14,23 +14,51 @@ const team_page = "team.html";
 let currentPage = "index.html";
 
 
-let arrayWithData = [];
+let players = [];
+let teams = [];
 
 let teamName;
 
 
-function starterFunction() {
+async function starterFunction() {
 	currentPage = window.location.pathname.split("/").find(str => str.includes(".html"));
 
     teamName = parseURLParams();
 
-	const teamsPromise = currentPage == team_page ? atlas.getPlayers(teamName) : atlas.getTeams();
-	teamsPromise
+
+    // om det är teampage skapas tabellen av players och team hämtas.
+    // else, tabellerna skapas av teams och players hämtas
+    if(currentPage == team_page){
+        const teamPromiste = await atlas.getTeams();
+        teams = teamPromiste;
+
+        const playerPromise = atlas.getPlayers();
+        playerPromise
 	.then(fetchedTeams => {
-		arrayWithData = fetchedTeams;
-		createTable();
+		players = fetchedTeams
+        createTable();
 	})
-	.catch(error => console.error(`An error occurd when getting teams from Atlas: ${error}`));
+    }
+    else{
+        const playerPromise = await atlas.getPlayers();
+        players = playerPromise;
+
+        const teamPromiste = atlas.getTeams();
+        teamPromiste
+	.then(fetchedTeams => {
+		teams = fetchedTeams
+        createTable();
+	})
+    }
+
+
+	// const teamsPromise = currentPage == team_page ? atlas.getPlayers() : atlas.getTeams();
+	// teamsPromise
+	// .then(fetchedTeams => {
+	// 	arrayWithData = fetchedTeams;
+	// 	createTable();
+	// })
+
 
     if(currentPage == team_page){
         teamName = parseURLParams();
@@ -54,22 +82,24 @@ function parseURLParams() {
 
 function createTable() {
 
-	const tableCreator = currentPage == team_page ? createTableForPlayers : createTableForTeams;
-
 	const table = document.getElementById("teams_table");
 	table.innerHTML = null;
 
 
     const selectElement = document.createElement("select");
-			selectElement.id = "select_" + arrayWithData.teamName;
+			selectElement.id = "select_" + teams.teamName;
 
-
-	tableCreator(arrayWithData, table);
+    if (currentPage == team_page){
+        createTableForPlayers(players, table)
+    }
+    else{
+        createTableForTeams(teams, table)
+    }
 }
 
-function createTableForPlayers(arrayWithData, table) {
+function createTableForPlayers(players, table) {
     
-	arrayWithData.forEach(team => {
+	players.forEach(team => {
         if (team.teamName == teamName){
             const tr = document.createElement("tr");
 
@@ -78,18 +108,59 @@ function createTableForPlayers(arrayWithData, table) {
             createTd(team.length, tr);
     
             table.appendChild(tr);
+
+            
         }
+        
 		
 	});
+
+    teams.forEach(team => {
+        if (team.teamName == teamName){
+            var element = document.getElementById('team-information');
+            var pTeamName = document.createElement('p');
+            var pCreated = document.createElement('p');
+            var pLocation = document.createElement('p');
+            var pArena = document.createElement('p');
+            var pSMGolds = document.createElement('p');
+            var pHeadCoach = document.createElement('p');
+            var pDivision = document.createElement('p');
+
+            pTeamName.innerHTML = 'Namn: ' + teamName;
+            pCreated.innerHTML = 'Grundad: ' + team.created;
+            pLocation.innerHTML = 'Stad: ' + team.location;
+            pArena.innerHTML = 'Arena: ' + team.arena;
+            pSMGolds.innerHTML = 'SM-guld: ' + team.SMgolds;
+            pHeadCoach.innerHTML = 'Huvudtränare: ' + team.headCoach;
+            pDivision.innerHTML = 'Division: ' + team.division;
+    
+            element.appendChild(pTeamName);
+            element.appendChild(pCreated);
+            element.appendChild(pLocation);
+            element.appendChild(pArena);
+            element.appendChild(pSMGolds);
+            element.appendChild(pHeadCoach);
+            element.appendChild(pDivision);
+
+            
+        }
+        
+		
+	});
+
+     
+
+    
+   
 }
 /**
 * Create table rows for all teams in the array.
-* @param arrayWithData an array of teams to create table rows for
+* @param teams an array of teams to create table rows for
 * @param table the table or the table body to add the rows to
 */
-function createTableForTeams(arrayWithData, table) {
+function createTableForTeams(teams, table) {
 	
-	arrayWithData.forEach(team => {
+	teams.forEach(team => {
 
 		const tr = document.createElement("tr");
 
