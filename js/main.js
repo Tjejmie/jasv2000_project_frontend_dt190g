00c55ef2c29c516f03399648d7ba7103708e402e
jamie <jamie_svanberg@hotmail.com>
2022-12-23 12:46:24 +0100

    Player page added with information

--- js/main.js
+++ js/main.js
@@ -7,23 +7,28 @@ const dataSource = new RESTDataSource("https://jasv2000-project-backend-dt190g.a
 /** The Atlas instance */
 const atlas = new Atlas(dataSource);
 
-/** The name of the page displaying the list of My courses */
+/** The name of the page displaying the list of teams */
 const team_page = "team.html";
+
+/** The name of the page displaying player */
+const player_page = "player.html";
 
 /** The name of the page curretly beeing displayed */
 let currentPage = "index.html";
+
+let startPage = "index.html";
 
 
 let players = [];
 let teams = [];
 
 let teamName;
+let playerName;
 
 
 async function starterFunction() {
 	currentPage = window.location.pathname.split("/").find(str => str.includes(".html"));
 
-    teamName = parseURLParams();
 
 
     // om det är teampage skapas tabellen av players och team hämtas.
@@ -31,7 +36,8 @@ async function starterFunction() {
     if(currentPage == team_page){
         const teamPromiste = await atlas.getTeams();
         teams = teamPromiste;
-
+        teamName = parseURLParams();
+        document.getElementById("teamName").innerHTML= teamName;
         const playerPromise = atlas.getPlayers();
         playerPromise
 	.then(fetchedTeams => {
@@ -39,7 +45,20 @@ async function starterFunction() {
         createTable();
 	})
     }
-    else{
+    else if(currentPage == player_page){
+        const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+        playerName = urlParams.get('player')
+        
+        document.getElementById("playerName").innerHTML= playerName;
+        const playerPromise = atlas.getPlayers();
+        playerPromise
+	.then(fetchedTeams => {
+		players = fetchedTeams
+        createPlayerInformation(players, playerName);
+	})}
+    else
+    {
         const playerPromise = await atlas.getPlayers();
         players = playerPromise;
 
@@ -49,22 +68,6 @@ async function starterFunction() {
 		teams = fetchedTeams
         createTable();
 	})
-    }
-
-
-	// const teamsPromise = currentPage == team_page ? atlas.getPlayers() : atlas.getTeams();
-	// teamsPromise
-	// .then(fetchedTeams => {
-	// 	arrayWithData = fetchedTeams;
-	// 	createTable();
-	// })
-
-
-    if(currentPage == team_page){
-        teamName = parseURLParams();
-
-        // title to the clicked teamname
-        document.getElementById("teamName").innerHTML= teamName;
     }
 
 	
@@ -97,24 +100,104 @@ function createTable() {
     }
 }
 
+function createPlayerInformation(players, playerName){
+    
+    for(let i = 0; i < players.length; i++){
+        if (players[i].name == playerName){
+            var element = document.getElementById('player-information');
+            var pName = document.createElement('p');
+            var pAge= document.createElement('p');
+            var pTeamName = document.createElement('p');
+            var pPosition = document.createElement('p');
+            var pNumber = document.createElement('p');
+            var pBirthPlace = document.createElement('p');
+            var pLenght = document.createElement('p');
+            var pWeight = document.createElement('p');
+            var pShooter = document.createElement('p');
+            var pYouthTeam = document.createElement('p');
+            var pContract = document.createElement('p');
+
+            // create image on top of page
+            var imageElement = document.getElementById('image');
+            var image = document.createElement('img');
+            fetch('/images/'+players[i].teamName+'.avif', { method: 'HEAD' })
+               .then(res => {
+                 if (res.ok) {
+                    image.src = '/images/'+players[i].teamName+'.avif';
+                  } else {
+                    image.src = '/images/ishockey.jpg';
+                }
+            }).catch(err => console.log('Error:', err));
+            imageElement.appendChild(image);
+
+            pName.innerHTML = 'Namn: ' + playerName;
+            pAge.innerHTML = 'Ålder: ' + players[i].born;
+            pTeamName.innerHTML = 'Lag: ' + players[i].teamName;
+            
+            pPosition.innerHTML = 'Position: ' + players[i].position;
+            pNumber.innerHTML = 'Tröjnummer: ' + players[i].number;
+            pBirthPlace.innerHTML = 'Nationalitet: ' + players[i].birthplace;
+            if (players[i].length != null){
+                pLenght.innerHTML = 'Längd: ' + players[i].length;
+            }
+            else{
+                pLenght.innerHTML = 'Längd: Ingen information';
+            }
+
+            if (players[i].weight != null){
+                pWeight.innerHTML = 'Vikt: ' + players[i].weight;
+            }
+            else{
+                pWeight.innerHTML = 'Vikt: Ingen information';
+            }
+            
+            if(players[i].shoots == null){
+                pShooter.innerHTML = 'Skjuter från: Ingen information'
+            }
+            else if (players[i].position == "Målvakt"){
+                pShooter.innerHTML = 'Plockar med: ' + players[i].shoots;
+            }
+            else{
+                pShooter.innerHTML = 'Skjuter från: ' + players[i].shoots;
+            }
+
+            if (players[i].youthTeam != null){
+                pYouthTeam.innerHTML = 'Ungdomslag: ' + players[i].youthTeam;
+            }
+            else{
+                pYouthTeam.innerHTML = 'Ungdomslag: Ingen information';
+            }
+            
+            pContract.innerHTML = 'Kontrakt till: ' + players[i].contract;
+            
+
+            element.appendChild(pName);
+            element.appendChild(pTeamName);
+            element.appendChild(pAge);
+            element.appendChild(pPosition);
+            element.appendChild(pNumber);
+            element.appendChild(pBirthPlace);
+            element.appendChild(pLenght);
+            element.appendChild(pWeight);
+            element.appendChild(pShooter);
+            element.appendChild(pYouthTeam);
+            element.appendChild(pContract);
+        };
+
+    }
+
+}
+
 function createTableForPlayers(players, table) {
 
-    
-    
 	players.forEach(team => {
         if (team.teamName == teamName){
             const tr = document.createElement("tr");
-
-            createTd(team.name, tr);
+            createTd(team.name, tr, element => element.innerHTML='<a class="nav-link" href="player.html?player='+team.name+'">'+team.name+'</a>');
             createTd(team.birthplace, tr);
             createTd(team.length, tr);
-    
             table.appendChild(tr);
-
-            
         }
-        
-		
 	});
 
     teams.forEach(team => {
@@ -129,9 +212,6 @@ function createTableForPlayers(players, table) {
             var pHeadCoach = document.createElement('p');
             var pDivision = document.createElement('p');
             var image = document.createElement('img');
-
-         
-         
           
             fetch('/images/'+teamName+'.avif', { method: 'HEAD' })
                .then(res => {
@@ -143,13 +223,32 @@ function createTableForPlayers(players, table) {
             }).catch(err => console.log('Error:', err));
 
             
-
             pTeamName.innerHTML = 'Namn: ' + teamName;
             pCreated.innerHTML = 'Grundad: ' + team.created;
             pLocation.innerHTML = 'Stad: ' + team.location;
-            pArena.innerHTML = 'Arena: ' + team.arena;
-            pSMGolds.innerHTML = 'SM-guld: ' + team.SMgolds;
-            pHeadCoach.innerHTML = 'Huvudtränare: ' + team.headCoach;
+
+            if (team.arena != null){
+                pArena.innerHTML = 'Arena: ' + team.arena;
+            }
+            else{
+                pArena.innerHTML = 'Arena: Ingen information';
+            }
+
+            if (team.SMgolds != null){
+                pSMGolds.innerHTML = 'SM-guld: ' + team.SMgolds;
+            }
+            else{
+                pSMGolds.innerHTML = 'SM-guld: Ingen information';
+            }
+            
+            if (team.headCoach != null){
+                pHeadCoach.innerHTML = 'Huvudtränare: ' + team.headCoach;
+            }
+            else{
+                pHeadCoach.innerHTML = 'Huvudtränare: Ingen information';
+            }
+   
+           
             pDivision.innerHTML = 'Division: ' + team.division;
             imageElement.appendChild(image);
             element.appendChild(pTeamName);
@@ -165,12 +264,7 @@ function createTableForPlayers(players, table) {
         }
         
 		
-	});
-
-     
-
-    
-   
+	}); 
 }
 /**
 * Create table rows for all teams in the array.
@@ -182,9 +276,6 @@ function createTableForTeams(teams, table) {
 	teams.forEach(team => {
 
 		const tr = document.createElement("tr");
-
-
-		//createTd(team.teamName, tr, element => element.addEventListener("click", (_) => click(team.teamName)));
         createTd(team.teamName, tr, element => element.innerHTML='<a class="nav-link" href="team.html?team='+team.teamName+'">'+team.teamName+'</a>');
 		createTd(team.location, tr);
 		createTd(team.division, tr);
@@ -193,12 +284,7 @@ function createTableForTeams(teams, table) {
 	});
 }
 
-// function click(teamName){
-//     localStorage.setItem('teamName', teamName);
 
-//     window.location.href = "team.html"
-  
-// }
 /**
 * Create a data cell (td element) with the specified text
 * @param text the text to to be displayed in the data cell
